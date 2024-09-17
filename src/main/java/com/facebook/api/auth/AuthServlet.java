e55e9d90c975ccab72049b9b9686db07bc9c33a7
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2024-09-17 23:31:31 +0530

    Cookie SESSIONID

--- src/main/java/com/facebook/api/auth/AuthServlet.java
+++ src/main/java/com/facebook/api/auth/AuthServlet.java
@@ -127,7 +127,7 @@ public class AuthServlet extends HttpServlet {
 		}
 		List<String> msg = new ArrayList<>();
 		msg.add("Successfully logout");
-		Cookie sessionCookie = new Cookie("user_id", "");
+		Cookie sessionCookie = new Cookie("SESSIONID", "");
 		sessionCookie.setMaxAge(0);
 		response.addCookie(sessionCookie);
 		response.setStatus(HttpServletResponse.SC_OK);
