c60e5277d7db2d724555005887da74e034a1224f
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2024-09-26 14:12:27 +0530

    profile image

--- src/main/java/com/facebook/api/user/Profile.java
+++ src/main/java/com/facebook/api/user/Profile.java
@@ -6,10 +6,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Profile {
 	@JsonProperty("image")
-	private String imageBase64;
+	private String image;
 
 	public Profile(byte[] image) {
 		super();
-		this.imageBase64 = (image != null) ? Base64.getEncoder().encodeToString(image) : null;
+		this.image = (image != null) ? Base64.getEncoder().encodeToString(image) : null;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 }
